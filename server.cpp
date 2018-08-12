72225aa8774b92fe1f89a6721e522f3edc03bf7c
incredible-john <zhang_shi_zhao@163.com>
2018-08-12 10:32:06 +0800

    删减了一些注释语句

--- server.cpp
+++ server.cpp
@@ -202,8 +202,6 @@ void server::disconnected(tcpsocket *clientsocket)
 //    free(clientsocket);                    //释放这个不用的连接的内存  此处要用free 不能用delete 会出错
 }
 
-//void server::paintEvent(QPaintEvent *)
-//{
 
 
     //如果lastpoint或endpoint没有数据，则return跳出函数
@@ -223,25 +221,6 @@ void server::disconnected(tcpsocket *clientsocket)
 //        }
 //    }
 
-
-    //在场景scene中添加新一段轨迹LineItem，同时将lineitem的数量+1，并用指针lineItemPointer记录
-    //lineItemPointer[lineItemNum++] = scene.addLine(lastpoint.x(),lastpoint.y(),endpoint.x(),endpoint.y());
-    //lineItemPointer[lineItemNum-1]->setPen(pen);
-//    qDebug() << "linepointer的数据" << endl << lineItemPointer[lineItemNum-1]->line().x2() << "  "
-//             << lineItemPointer[lineItemNum-1]->line().y2() <<endl;
-
-//    if(lineItemNum>20)
-//    {
-//        scene.removeItem(lineItemPointer[0]);
-//        for(int j=0;j<lineItemNum-1;j++)
-//        {
-//            lineItemPointer[j] = lineItemPointer[j+1];
-//        }
-//        lineItemNum--;
-//    }
-
-    //ui->graphicsView->setScene(&scene);//把场景添加到ui中GraphicsView的框图中
-//}
 
 void server::wificonnected()
 {
